6c348cb354d54966146ce902059d05d4984228e1
adinica <adinica@deloittece.com>
2023-07-21 10:19:30 +0300

    Else exercise and Operator

---
+++ src/main/java/OperatorTernary.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class OperatorTernary {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("What day is it?");
+        String  day = scanner.nextLine();
+        String howToSpendTime;
+
+//        if (day.toLowerCase().equals("saturday")) {
+//            howToSpendTime = "Relax";
+//        }else {
+//            howToSpendTime = "Work";
+//        }
+        howToSpendTime = day.toLowerCase().equals("saturday")? "Relax" : "Work";
+
+        System.out.printf("%s on %s %n",howToSpendTime,day);
+        scanner.close();
+    }
+}
---
+++ src/main/java/excerciseIfElse.java
@@ -0,0 +1,10 @@
+public class excerciseIfElse {
+    public static void main(String[] args){
+        Object obj1 = new Object();
+        Object obj2 = obj1;
+        if( obj1.equals(obj2) )
+            System.out.println("true");
+        else  System.out.println("false");
+    }
+
+}
