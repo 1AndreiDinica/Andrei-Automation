5800ff63dae7f2581b98ef92e0f8f1000a7e0535
adinica <adinica@deloittece.com>
2023-07-25 15:47:57 +0300

    Excercises

---
+++ src/main/java/ArithmeticProgression.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class ArithmeticProgression {
+
+    public static void main(String[] args){
+        Scanner number= new Scanner(System.in);
+
+        System.out.println("AP Series");
+        System.out.println("Enter a, d and n");
+
+        int a = number.nextInt();
+        int d = number.nextInt();
+        int n = number.nextInt();
+
+        int term = a;
+        for(int i=0; i<n;i++)
+        {
+           System.out.print(term+ ",");
+           term=term+d;
+        }
+
+
+    }
+}
