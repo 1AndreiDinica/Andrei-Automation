bced2457404dc83aef8e0fb30a01dc5180988a61
adinica <adinica@deloittece.com>
2023-07-30 22:10:32 +0300

    Coding Bat

---
+++ src/main/java/conCat.java
@@ -0,0 +1,27 @@
+public class conCat {
+
+    public static void main(String[] args){
+
+        String aplha = "cat";
+        String beta = "tank";
+
+
+        String firstchar = aplha.substring(aplha.length()-1);
+        String lastchar = beta.substring(0,1);
+
+        if (firstchar.equals(lastchar))
+        {
+
+            System.out.println(aplha.substring(0,aplha.length()-1)+beta);
+
+        } else {
+            System.out.printf(aplha + beta);
+        }
+
+
+
+
+
+    }
+
+}
---
+++ src/main/java/frontAgain.java
@@ -0,0 +1,20 @@
+public class frontAgain {
+
+    public static void main (String[] args) {
+
+
+        String frontAgain ="ratara";
+
+
+        String firstTwo = frontAgain.substring(0,2);
+        String lasttwo = frontAgain.substring(frontAgain.length()-2);
+
+        System.out.println(firstTwo.equals(lasttwo));
+
+
+
+
+    }
+
+
+}
