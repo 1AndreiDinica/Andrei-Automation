f365d52febdf594c20d5ba264ab7eeb8db69fecc
adinica <adinica@deloittece.com>
2023-07-31 11:24:28 +0300

    Coding Bat

---
+++ src/main/java/comboString.java
@@ -0,0 +1,29 @@
+public class comboString {
+
+    public static void main (String[] args ){
+
+
+        String a = "";
+
+        String b = "";
+
+
+        if (b.length()<a.length()){
+
+
+            System.out.println(b+a+b);
+        }else if (a.length()<b.length()){
+
+            System.out.println(a+b+a);
+
+        }else if (a.length()==0 && b.length() ==0){
+
+            System.out.println("Both Strings are Empty");
+        }
+
+
+        }
+
+
+    }
+
